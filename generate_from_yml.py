80104b75edf013f73e4268a32eeacc6a7b6df369
pimmen <pontus89ekholm@gmail.com>
2018-08-01 10:11:00 +0200

    Hotfix: Updated faulty category codes and test script

--- generate_from_yml.py
+++ generate_from_yml.py
@@ -5,20 +5,29 @@ import json
 yaml_file = open('src/categories-coded.yml', 'r')
 categories_dict = yaml.load(yaml_file)
 
-category_codes = set([c['code'] for c in categories_dict['categories']])
+print("Checking category tree format.")
 
+category_codes = set([c['code'] for c in categories_dict['categories']])
 
 if len(categories_dict['categories']) != len(category_codes):
 	sys.exit("Error! Category codes need to be unique!")
 
-category_codes_tokenized = [c.split('-') for c in category_codes]
+for category_code in category_codes:
+	last_hyphen_position = category_code.rfind("-")
 
-for category_tokens in category_codes_tokenized:
-	parent_tokens = category_tokens[0:(len(category_tokens)-2)]
-	if parent_tokens:
-		if parent_tokens not in category_codes_tokenized:
-			category_code = '-'.join(category_tokens)
-			sys.exit("Error! " + category_code + " does not map to a parent category!")
+	if last_hyphen_position == -1:
+		continue
+
+	parent_code = category_code[0:last_hyphen_position]
+
+	#print(category_code, parent_code)
+
+	if parent_code not in category_codes:
+		sys.exit("Error! " + category_code + " does not map to a parent category!")
+
+print("Category tree is in correct format! Exporting YAML to JSON.")
 
 with open('dist/categories-coded.json', 'w', encoding='utf-8') as json_file:
 	json.dump(categories_dict, json_file, indent=4, ensure_ascii=False)
+
+print("Export completed!")
