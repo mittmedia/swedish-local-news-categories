1dc8c082b67778731f3732c798de4a938fd2a27e
Jon-Erik Johnzon <jon-erik.johnzon@mittmedia.se>
2019-02-12 10:25:31 +0100

    fixing bugs in category_tool.py

--- category_tool.py
+++ category_tool.py
@@ -1,27 +1,29 @@
 import yaml
 
+
 def load_categories():
-    with open('src/categories-coded.yml', 'r') as categories_file:
+    with open('src/categories-coded.yml', 'r', encoding='utf-8') as categories_file:
         categories_dict = yaml.load(categories_file)
 
     for k in categories_dict['categories']:
-        for j,v in k.items():
-            print( "{} >> {}".format(j,v))
+        for j, v in k.items():
+            print("{} >> {}".format(j, v))
 
     return categories_dict
 
 
 def save_categories(categories):
-    with open('src/categories-coded.yml', 'w') as categories_file:
+    with open('src/categories-coded.yml', 'w', encoding='utf-8') as categories_file:
         yaml.dump(categories, categories_file)
 
+
 def load_modfile():
-    with open('diff-example.yml', 'r') as modfile:
+    with open('diff-example.yml', 'r', encoding='utf-8') as modfile:
         mod_dict = yaml.load(modfile)
 
     for k in mod_dict['modifications']:
-        for j,v in k.items():
-            print( "{} >> {}".format(j,v))
+        for j, v in k.items():
+            print("{} >> {}".format(j, v))
 
     return mod_dict
 
@@ -36,8 +38,11 @@ def update_categories(categories, mod_operation):
                     k['description'] = mod_operation['description']
     return categories
 
+
 if __name__ == '__main__':
     categories = load_categories()
     modifications = load_modfile()
-    save_categories(update_categories(categories,modifications))
+    for mod in modifications['modifications']:
+        update_categories(categories, mod)
 
+    save_categories(categories)
