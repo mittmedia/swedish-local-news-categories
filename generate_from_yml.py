fb2bf55136cb19bdec159fb9a734265cd54e6f95
Jon-Erik Johnzon (TorrentKatten) <jone@torrentkatten.se>
2019-02-27 14:38:23 +0100

    fixing encoding issue

--- generate_from_yml.py
+++ generate_from_yml.py
@@ -1,8 +1,9 @@
-import sys
-import yaml
 import json
+import sys
 
-yaml_file = open('src/categories-coded.yml', 'r')
+import yaml
+
+yaml_file = open('src/categories-coded.yml', 'r', encoding='utf-8', newline='\n')
 categories_dict = yaml.load(yaml_file)
 
 print("Checking category tree format.")
@@ -10,29 +11,29 @@ print("Checking category tree format.")
 category_codes = set([c['code'] for c in categories_dict['categories']])
 
 if len(categories_dict['categories']) != len(category_codes):
-	sys.exit("Error! Category codes need to be unique!")
+    sys.exit("Error! Category codes need to be unique!")
 
 for category in categories_dict['categories']:
-	tokenized_code = category['code'].split('-')
-	if (category['level'] + 1) != len(tokenized_code):
-		sys.exit("Level for category " + category['code'] + " is incorrect!")
+    tokenized_code = category['code'].split('-')
+    if (category['level'] + 1) != len(tokenized_code):
+        sys.exit("Level for category " + category['code'] + " is incorrect!")
 
 for category_code in category_codes:
-	last_hyphen_position = category_code.rfind("-")
+    last_hyphen_position = category_code.rfind("-")
 
-	if last_hyphen_position == -1:
-		continue
+    if last_hyphen_position == -1:
+        continue
 
-	parent_code = category_code[0:last_hyphen_position]
+    parent_code = category_code[0:last_hyphen_position]
 
-	#print(category_code, parent_code)
+    # print(category_code, parent_code)
 
-	if parent_code not in category_codes:
-		sys.exit("Error! " + category_code + " does not map to a parent category!")
+    if parent_code not in category_codes:
+        sys.exit("Error! " + category_code + " does not map to a parent category!")
 
 print("Category tree is in correct format! Exporting YAML to JSON.")
 
-with open('dist/categories-coded.json', 'w', encoding='utf-8') as json_file:
-	json.dump(categories_dict, json_file, indent=4, ensure_ascii=False)
+with open('dist/categories-coded.json', 'w', newline='\n', encoding='utf-8') as json_file:
+    json.dump(categories_dict, json_file, indent=4, ensure_ascii=False)
 
 print("Export completed!")
